5340067467773edc318e93ff5e68c355e8577e96
bruno44rus <kirrrill007@mail.ru>
2019-11-09 19:48:12 +0300

    The beginning of spider OLD

--- NewFile.py
+++ NewFile.py
@@ -1,1 +1,7 @@
-print('hello')
+import urllib.request
+url1 ='http://evil.bigazzzz.ru:15073/test.html'
+page = urllib.request.urlopen(url1)
+page1 = str(page.read().decode('utf-8'))
+file = open("file1.txt", "w")
+file.write(page1)
+file.close()
